e86b6670b60cc9b78505edc6e7df06db2ffb6ea6
Pedro Ap <pedroaparecidori@gmail.com>
2024-01-28 20:14:10 -0300

    Criado estados no gameOption

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react'
 import styles from './Game.module.css'
 import GameOption from '../cameoption/GameOption'
 
 function Game() {
+
+    const [gameState, setGameState] = useState(Array(9).fill(0))
+
     return(
         <div className={styles.game}>
+            {
+                gameState.map((value, pos) => 
+                    <GameOption 
+                        key={`game-option-pos-${pos}`}
+                        status={value}
+                    />
+                )
+            }
             <GameOption />
             <GameOption />
             <GameOption />
--- src/components/cameoption/GameOption.jsx
+++ src/components/cameoption/GameOption.jsx
@@ -1,9 +1,17 @@
 import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption() {
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25px" />
+
+function GameOption(props) {
     return(
         <div className={styles.gameOption}>
-
+            {
+                props.status === 1 && <Icon iconName="circle" size="25px" />
+            }
+            {
+                props.status === -1 && <Icon iconName="x" size="25px" />
+            }
         </div>
     )
 }
